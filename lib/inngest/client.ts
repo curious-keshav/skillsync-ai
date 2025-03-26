5ae471e236c8c11b9a244f91085e0039a021aca0
Keshav Verma <keshavverma532@gmail.com>
2025-03-26 15:53:03 +0530

    Added interview prep

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,5 +2,10 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
     id: "skillsync-ai",
-    name: "skillsync-ai"
+    name: "skillsync-ai",
+    credentials: {
+        gemini: {
+            apiKey: process.env.GEMINI_API_KEY
+        }
+    }
 })

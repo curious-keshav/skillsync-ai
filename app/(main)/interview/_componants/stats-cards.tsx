7a40de66730554dd9265201d5e450cc9b434f8af
Keshav Verma <keshavverma532@gmail.com>
2025-03-27 03:54:15 +0530

    Corrected stats-card

--- app/(main)/interview/_componants/stats-cards.tsx
+++ app/(main)/interview/_componants/stats-cards.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Brain, Target, Trophy } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";

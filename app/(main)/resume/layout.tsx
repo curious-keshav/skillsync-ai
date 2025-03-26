5723e37dea6fddccccb082aa611a8c2ef4d4fa32
Keshav Verma <keshavverma532@gmail.com>
2025-03-27 03:32:58 +0530

    Added resume and cover letter

---
+++ app/(main)/resume/layout.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { Suspense } from 'react'
+import { BarLoader } from "react-spinners"
+
+const Layout = ({ children }: { children: any }) => {
+    return (
+        <div className='px-5'>
+            <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color="gray"/>}>
+                {children}
+            </Suspense>
+        </div>
+    )
+}
+
+export default Layout
---
+++ app/lib/helper.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function entriesToMarkdown(entries, type) {
+    if (!entries?.length) return "";
+  
+    return (
+      `## ${type}\n\n` +
+      entries
+        .map((entry:any) => {
+          const dateRange = entry.current
+            ? `${entry.startDate} - Present`
+            : `${entry.startDate} - ${entry.endDate}`;
+          return `### ${entry.title} @ ${entry.organization}\n${dateRange}\n\n${entry.description}`;
+        })
+        .join("\n\n")
+    );
+  }
+  
